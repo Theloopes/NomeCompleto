3605eeeb0c8bab0a223b21665c65e0f8b52246ca
Theloopes <e107ti@etec.sp.gov.br>
2023-08-18 20:43:01 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string nome;
+string sobrenome;
+
+Console.WriteLine("Digite seu primeiro nome");
+nome = Console.ReadLine() !;
+Console.WriteLine("Digite seu sobrenome");
+sobrenome = Console.ReadLine() !;
+
+Console.Write(nome);
+Console.Write(" ");
+Console.WriteLine(sobrenome);
+Thread.Sleep(500);
+
+Console.WriteLine();
+
+Console.Write(sobrenome.ToUpper());
+Console.Write(", ");
+Console.WriteLine(nome);
+
